915d7b19feb27fbf3dbce1b030551bbf7242ec5a
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2024-09-21 20:31:42 +0200

    added filter by region button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+  import * as Select from "$lib/components/ui/select";
+</script>
+
 <main class="bg-light-bg dark:bg-dark-bg px-6 py-8">
   <div
-    class="flex items-center shadow-xl dark:outline dark:outline-[.8rem] dark:outline-outline-dark justify-center gap-6 py-5 px-8 rounded-md bg-white dark:bg-dark-el"
+    class="flex items-center shadow-xl dark:outline dark:outline-[.8rem] dark:outline-outline-dark justify-center gap-6 py-5 px-8 mb-8 rounded-md bg-white dark:bg-dark-el"
   >
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -23,5 +27,16 @@
     />
   </div>
 
-  <div></div>
+  <Select.Root>
+    <Select.Trigger class="py-8 w-[55vw] shadow-xl">
+      <Select.Value class="px-2" placeholder="Filter by Region" />
+    </Select.Trigger>
+    <Select.Content>
+      <Select.Item value="africa">Africa</Select.Item>
+      <Select.Item value="america">America</Select.Item>
+      <Select.Item value="asia">Asia</Select.Item>
+      <Select.Item value="europe">Europe</Select.Item>
+      <Select.Item value="oceania">Oceania</Select.Item>
+    </Select.Content>
+  </Select.Root>
 </main>
