4acad663661d124dbd56f4af7fb0673478ae7a73
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2024-09-22 20:40:45 +0200

    fetch API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import * as Select from "$lib/components/ui/select";
+  //import { error } from "@sveltejs/kit";
+  let { data } = $props();
+  const { countries } = $derived(data);
 </script>
 
 <main class="bg-light-bg dark:bg-dark-bg px-6 py-8">
@@ -28,10 +31,16 @@
   </div>
 
   <Select.Root>
-    <Select.Trigger class="py-8 w-[55vw] shadow-xl">
+    <Select.Trigger
+      class="py-8 w-[55vw] shadow-xl dark:placeholder:text-white
+dark:bg-dark-el font-semibold text-base"
+    >
       <Select.Value class="px-4" placeholder="Filter by Region" />
     </Select.Trigger>
-    <Select.Content>
+    <Select.Content
+      class="dark:placeholder:text-white
+dark:bg-dark-el"
+    >
       <Select.Item value="africa">Africa</Select.Item>
       <Select.Item value="america">America</Select.Item>
       <Select.Item value="asia">Asia</Select.Item>
@@ -39,4 +48,26 @@
       <Select.Item value="oceania">Oceania</Select.Item>
     </Select.Content>
   </Select.Root>
+
+  <div>
+    {#await countries}
+      <p>Fetching Countries...</p>
+    {:then countries}
+      <div>
+        <ul>
+          {#each countries as country}
+            <li>
+              <div>
+                <img src={country.flag} alt="the flag of {country.name}" />
+              </div>
+              <h2>{country.name}</h2>
+              <p>{country.population}</p>
+              <p>{country.region}</p>
+              <p>{country.capital}</p>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    {/await}
+  </div>
 </main>
---
+++ src/routes/+page.ts
@@ -0,0 +1,12 @@
+export const load = async ({ fetch }) => {
+  try {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    if (!res.ok) {
+      throw new Error(`Failed to fetch status: ${res.status}`);
+    }
+    const countries = res.json();
+    return { countries };
+  } catch (error) {
+    console.log("Failed to load countries", error);
+  }
+};
