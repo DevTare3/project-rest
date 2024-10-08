60bb018950061d49db83459e9e7400542b0a439b
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2024-10-08 18:13:48 +0200

    addeed apu call

--- src/routes/detailed/[countryName]/+page.ts
+++ src/routes/detailed/[countryName]/+page.ts
@@ -1,12 +1,29 @@
-export const load = ({ fetch, params }) => {
+export const load = ({ fetch, params }: any) => {
   const fetchCountry = async (countryID: any) => {
     const res = await fetch(
       `https://restcountries.com/v3.1/alpha/${countryID}`
     );
-    const data = await res.json();
-    return data;
+    const data = (await res.json()) as any;
+    let borderCountries: any[] = [];
+    if (data.borders) {
+      console.log("in borders");
+      const fetchBorderCountries = async (borderCountryID: any) => {
+        const res = await fetch(
+          `https://restcountries.com/v3.1/alpha/${borderCountryID}`
+        );
+        const borderCountryData = await res.json();
+        console.log("here it is" + borderCountryData);
+        return borderCountryData;
+      };
+      borderCountries = await Promise.all(
+        data.borders.map((element: any) => fetchBorderCountries(element.cca3))
+      );
+    }
+    return {
+      country: data,
+      borderCountries: borderCountries,
+    };
   };
-  return {
-    country: fetchCountry(params.countryName),
-  };
+
+  return fetchCountry(params.countryName);
 };
