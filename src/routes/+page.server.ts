0aaf4f20592239bf0a91427e399736d488679732
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2024-10-19 19:23:27 +0200

    lets commit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,12 @@
 export const actions = {
-    setTheme: async({url,cookies}) => {
-        const theme = url.searchParams.get('theme')
-        
+  setTheme: async ({ url, cookies }) => {
+    const theme = url.searchParams.get("theme");
 
-        if(theme === 'dark' || theme === 'light')
-        {
-            cookies.set("colortheme",theme, {
-                path: "/", 
-                maxAge : 60 * 60 * 365 * 24,
-            });
-        }
+    if (theme === "dark" || theme === "light") {
+      cookies.set("colortheme", theme, {
+        path: "/",
+        maxAge: 60 * 60 * 365 * 24,
+      });
     }
-}
+  },
+};
